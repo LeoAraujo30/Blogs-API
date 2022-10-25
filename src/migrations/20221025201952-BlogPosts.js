988e494a22a0cd778b1dd1b9a06d9901abdf7bb3
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-10-25 18:02:40 -0300

    requisito 1 feito

---
+++ src/migrations/20221025201952-BlogPosts.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      title: { type: Sequelize.STRING, allowNull: false },
+      content: { type: Sequelize.STRING, allowNull: false },
+      user_id: { 
+        type: Sequelize.INTEGER,
+        references: { model: 'users',  key: 'id' },
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: { type: Sequelize.DATE, allowNull: false },
+      updated: { type: Sequelize.DATE, allowNull: false },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
