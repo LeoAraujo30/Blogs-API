042b5b6282feba34ae5621629ea04f4daa509a3c
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-10-26 16:29:39 -0300

    requisito 11 feito

---
+++ src/models/PostCategory.js
@@ -0,0 +1,25 @@
+module.exports = (Sequelize, DataTypes) => {
+  const PostCategory = Sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
+  },
+  { timestamps: false, tableName: 'posts_categories', underscored: true });
+    
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      as: 'categories',
+      through: PostCategory,
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      as: 'posts',
+      through: PostCategory,
+    });
+  }
+    
+  return PostCategory;
+};
+    
