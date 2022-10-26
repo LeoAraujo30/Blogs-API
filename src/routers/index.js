590aa711d6337379a8cff2eba4cd3dd433b3230e
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-10-26 15:08:13 -0300

    requisito 7 feito

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const loginRouter = require('./loginRouter');
 const userRouter = require('./userRouter');
+// const { validateToken } = require('../middlewares/validateToken');
 
 const router = (express.Router());
 
 router.use('/login', loginRouter);
 router.use('/user', userRouter);
+// router.use(validateToken);
 // router.use('/categories');
 // router.use('/post');
 
